7de895cc22635df3813b931513b39eb58d48c896
Marcelo Schmidt <marcelo.schmidt@gmail.com>
2012-08-29 14:02:14 -0400

    Template files for a new dojo

---
+++ dojo_php_start/src/Template.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * DojoException class may be used to test general exceptions;
+ * PHPUnit does not allow one to test against the Exception class.
+ * 
+ * @author "Marcelo Schmidt" <marcelo.schmidt@gmail.com>
+ */
+class DojoException extends Exception {};
+
+/*
+ * This is a template class, which should be modified to match
+ * your needs.
+ * The method run() is called from test/TemplateTest.php.
+ *
+ * @author "Marcelo Schmidt" <marcelo.schmidt@gmail.com>
+ */
+class Template {
+	public function run() {
+
+	}
+}
